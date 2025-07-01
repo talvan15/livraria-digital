24ce4ed6fab1648993ad29304cc55463e9732652
Talvan <talvan.alves00@gmail.com]>
2025-07-01 14:40:44 -0300

    Atualizando o css das categorias

--- app/(tabs)/categories.tsx
+++ app/(tabs)/categories.tsx
@@ -33,10 +33,12 @@ export default function categoriesScreen() {
       <View
         style={{
           backgroundColor: item.color,
-          padding: 20,
+          padding: 30,
           marginVertical: 10,
           borderRadius: 10,
           width: "100%",
+          alignItems: "center",
+          justifyContent: "center",
         }}
       >
         <Text>
@@ -61,8 +63,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center",
     backgroundColor: "#f8f9fa",
+    marginHorizontal: 30,
   },
 
   text: {
